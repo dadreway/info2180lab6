0ca177ead1967093c4715a3f76f6cc0aa5653e02
Andrew Hylton <andrew.hylton02@mymona.uwi.edu>
2016-11-09 21:23:58 +0000

    completed exercise 3

--- dascript.js
+++ dascript.js
@@ -2,19 +2,15 @@
         
         function GetDocument()
         {
-            var lookup = document.getElementById("word").value.toLowerCae();
-            var httprequest = new XMLHttpRequest();
+            var lookup = document.getElementById("word").value.toLowerCase();
             var tochange = document.getElementById("result");
+            var httprequest = new XMLHttpRequest();
             
             httprequest.onreadystatechange = function()
             {
-                if (httprequest.readyState === httprequest.DONE)
+                if (this.readyState === 4 && this.status == 200)
                 {
-                    if(httprequest.status === 200)
-                    {
-                        tochange.innerHTML = httprequest.responseText;
-                        
-                    }
+                        tochange.innerHTML = this.responseText;
                 }
             };
             httprequest.open("GET", "request.php?q="+lookup, true);
